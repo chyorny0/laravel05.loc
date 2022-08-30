5ea44bcac5ca9f4362634b01dd740c914acea1c2
4e1ik <Artemi324@tut.by>
2022-08-30 11:33:09 +0300

    first comit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\MyController;
+use App\Http\Controllers\Admin\CategoryController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $category = \App\Models\Category::find(1);
+    dump($category);
+    return view('welcome');
+});
+
+Auth::routes();
+
+
+
+
+
+Route::prefix('admin')->group(function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::get('', [MyController::class, 'index'])->name('');
+
+    Route::resource('categories', CategoryController::class)->except(['show']);
+});
+
+
+
+
