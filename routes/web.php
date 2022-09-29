1765e3bb360661031067ce65e26ad2445291d737
chyorny0 <chyorny0.0@gmail.com>
2022-09-29 15:49:00 +0300

    __

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ use Illuminate\Support\Facades\Storage;
 */
 
 Route::get('/', SiteController::class);
-Route::get('/store', [SiteController::class, 'store']);
 Route::get("/cart", [CartController::class,"getCart"])->name("cart");
 Route::post("/add_to_cart", [CartController::class,"addToCart"])->name("addToCart");
 
