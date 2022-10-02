304ed2f8ff466ca8bc42c567fcf9ac8887233532
chyorny0 <chyorny0.0@gmail.com>
2022-10-02 16:12:21 +0300

    add hw_03.10

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryCollection;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new CategoryCollection(Category::all());
+    }
+
+    public function show(Category $category)
+    {
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Category::create($request->all());
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $category->fill($request->all())->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get("/users",[\App\Http\Controllers\Api\UserController::class,"index"]);
 Route::get("/users/{id}",[\App\Http\Controllers\Api\UserController::class,"show"]);
+
+//Route::prefix('category')->group(callback: function(){
+//
+//    Route::get("/",[\App\Http\Controllers\Api\CategoryController::class,"index"]);
+//    Route::get("/show/{id}",[\App\Http\Controllers\Api\CategoryController::class,"show"]);
+//    Route::post("/store",[\App\Http\Controllers\Api\CategoryController::class,"store"]);
+//    Route::post("/update/{id}",[\App\Http\Controllers\Api\CategoryController::class,"update"]);
+//    Route::post("/destroy/{id}",[\App\Http\Controllers\Api\CategoryController::class,"destroy"]);
+//
+//});
+
+Route::resource('category', CategoryController::class);
